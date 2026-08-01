d6d570575187834222ee4c2de141dfd5014a4600
Abdi Progress <yoliymepoh@gmail.com>
2026-08-01 19:51:59 +0300

    Improve Etubers about portrait

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -44,7 +44,7 @@ const About = () => {
           <div className="about-visual-column">
             <div className="about-image-wrapper">
               <img 
-                src="/images/etubers/thumbnails/video-editing-course.jpg" 
+                src="/images/etubers/thumbnails/facebook-ads-course.jpg" 
                 alt="Mikiyas - Etubers" 
                 className={`about-portrait ${imgError ? 'hidden' : ''}`}
                 onError={() => setImgError(true)}
